0904db943c20e52aea408382559887080515a44b
Jordan Hargreaves <jhargreaves231@gmail.com>
2021-01-27 19:27:35 -0800

    grab the first and last characters of the sentence.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 // Business logic:
 
+function firstAndLast(str) {
+
+  let n = str.length; 
+  charFirst = str.charAt(0); 
+  charLast = str.charAt(n - 1); 
+  console.log(charFirst)
+  console.log(charLast)
+} 
+
+
 // UI logic:
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
     const input = $("input#sentence").val();
-    $(".sentenceInput").text(input);
+    $(".sentenceInput").text(firstAndLast(input));
     $("#reveal").show();
   });
 });
