3ee77d18191a4ba350b3e3293979d1218eb3b34d
Jordan Hargreaves <jhargreaves231@gmail.com>
2021-01-28 18:55:03 -0800

    add the fourth function.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@
 function firstAndLast(str) {
 
   let n = str.length; 
-  charFirst = str.charAt(0).toUpperCase(); 
-  charLast = str.charAt(n - 1).toUpperCase(); 
+  let charFirst = str.charAt(0).toUpperCase(); 
+  let charLast = str.charAt(n - 1).toUpperCase(); 
 
   return [charFirst, charLast]
 } 
@@ -15,15 +15,50 @@ function reverse(array) {
   return reversed
 }
 
+function numberThree(variable) {
+  const input = $("input#sentence").val();
+  const newSentence = input.concat(variable)
+
+ return newSentence
+}
+
+function numberFour(str) {
+  const n = str.length;
+  const index = n/2;
+  const indexTwo = Math.round(index);
+  const output = str.charAt(indexTwo);
+
+  return output
+}
+
 // UI logic:
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
+
     const input = $("input#sentence").val();
     $(".sentenceInput").text(firstAndLast(input));
+
     const firstFunctionResult = firstAndLast(input);
-    //console.log(firstFunctionResult);
+    console.log(firstFunctionResult);
+
     $(".sentenceInputTwo").text(reverse(firstFunctionResult));
+
+    //const secondFunctionResult = reverse(firstFunctionResult);
+    const secondFunctionResult = firstFunctionResult.reverse();
+    console.log(secondFunctionResult);
+  
+    $(".sentenceInputThree").text(numberThree(secondFunctionResult));
+
+    const thirdFunctionResult = numberThree(secondFunctionResult);
+    console.log(thirdFunctionResult);
+
+    const fourthFunctionResult = numberFour(input);
+    console.log(fourthFunctionResult);
+
+    $(".sentenceInputFour").text(numberFour(input));
+
+
     $("#reveal").show();
   });
 });
