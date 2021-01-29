607cad9a8ce75e2ccf6a81f3753bc2821bc693cf
Jordan Hargreaves <jhargreaves231@gmail.com>
2021-01-28 16:37:28 -0800

    add a second function to page that reverses the results of function one.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,12 @@ function firstAndLast(str) {
   return [charFirst, charLast]
 } 
 
+function reverse(array) {
+  const reversed = array.reverse();
+
+  return reversed
+}
+
 // UI logic:
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
@@ -17,6 +23,7 @@ $(document).ready(function(){
     $(".sentenceInput").text(firstAndLast(input));
     const firstFunctionResult = firstAndLast(input);
     //console.log(firstFunctionResult);
+    $(".sentenceInputTwo").text(reverse(firstFunctionResult));
     $("#reveal").show();
   });
 });
